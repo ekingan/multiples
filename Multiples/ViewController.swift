fa5922fafc472a42c62256df481e29e2204c16aa
ekingan <ekingan@gmail.com>
2016-06-01 21:31:37 -0700

    game completed

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,15 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //properties
+    
+    var maxMultiple: Int = 500
+    var x: Int = 0
+    var y: Int = 0
+    var z: Int = 0
+    
+    
+    //outlets
+    
+    @IBOutlet weak var multiplesLogo: UIImageView!
+    @IBOutlet weak var numberText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func onPlayButtonClick(sender: UIButton!) {
+        
+        if numberText.text != nil && numberText.text != "" {
+            
+            multiplesLogo.hidden = true
+            numberText.hidden = true
+            playButton.hidden = true
+            
+            addButton.hidden = false
+            resultsLabel.hidden = false
+            
+            y = Int(numberText.text!)!
+            x = 0
+            z = (x + y)
+            
+            resultsLabel.text = "\(x) + \(y) = \(z)"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            
+            
+        }
+        
     }
+    
+    
+    @IBAction func onAdd() {
+        
+        x = y
+        y = (y+y)
+        z = (x + y)
+        
+        resultsLabel.text = "\(x) + \(y) = \(z)"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if isGameOver() {
+            restartGame()
+        }
+        
+    }
+    
+    
+    func isGameOver() -> Bool {
+        if z >= maxMultiple {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        z = 0
+        y = 0
+        z = 0
+        
+        numberText.text = ""
+        multiplesLogo.hidden = false
+        numberText.hidden = false
+        playButton.hidden = false
+        
+        addButton.hidden = true
+        resultsLabel.hidden = true
     }
 
 
